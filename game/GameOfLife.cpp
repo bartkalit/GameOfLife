07ef38abb01259833ebf96405819c6f053f05b1f
Bart <bartkalit@gmail.com>
2022-04-09 16:01:50 +0200

    restructure project

---
+++ game/GameOfLife.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <string>
+#include "board/Board.h"
+using namespace std;
+
+int main()
+{
+    int x = 800;
+    int y = 800;
+    std::string title = "GameOfLife";
+    /*cout << "Width should measure ";
+    cin>> x;
+    cout << "Height should measure ";
+    cin >> y;
+    cout << "The name should be ";
+    cin >> title;*/
+    Board board(x, y ,title, 10);
+    board.Start_game();
+    return 0;
+}
